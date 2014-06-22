0583f5a0c0bbb281b391f606eb92e30e698566fb
aha00a <aha00a@gmail.com>
2014-06-22 20:40:22 +0900

    change orientation

--- java-common-android/src/common/android/ActivityDev.java
+++ java-common-android/src/common/android/ActivityDev.java
@@ -3,6 +3,7 @@ package common.android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import common.android.extensions.ActivityBase;
@@ -14,6 +15,7 @@ public class ActivityDev extends ActivityBase {
         super.onCreate(savedInstanceState);
 
         LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
         setContentView(linearLayout);
 
         for(final Class clazz : new Class[] {
@@ -29,7 +31,7 @@ public class ActivityDev extends ActivityBase {
                     startActivity(new Intent(ActivityDev.this, clazz));
                 }
             });
-            linearLayout.addView(button);
+            linearLayout.addView(button, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         }
     }
 
