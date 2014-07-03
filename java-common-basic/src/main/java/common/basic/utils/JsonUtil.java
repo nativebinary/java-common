294d84e72b4341c89236cffd474b58d8c6e86b2a
aha00a <aha00a@gmail.com>
2014-07-04 01:36:57 +0900

    add interface

--- java-common-basic/src/main/java/common/basic/utils/JsonUtil.java
+++ java-common-basic/src/main/java/common/basic/utils/JsonUtil.java
@@ -1,8 +1,19 @@
 package common.basic.utils;
 
 public class JsonUtil {
+    public static interface IJson {
+        String toJson(Object o);
+    }
+
+    static IJson json = new IJson() {
+        @Override
+        public String toJson(Object o) {
+            return GsonUtil.toJson(o);
+//            return Json.stringify(Json.toJson(o));
+        }
+    };
 
     public static String toJsonString(Object o) {
-        return GsonUtil.toJson(o);
+        return json.toJson(o);
     }
 }
