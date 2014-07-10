6ce1ae88c2137bd3d22114dd7455cb82ac60d988
aha00a <aha00a@gmail.com>
2014-07-10 15:33:35 +0900

    inline vars

--- java-common-basic/src/main/java/common/basic/jsons/GsonUtil.java
+++ java-common-basic/src/main/java/common/basic/jsons/GsonUtil.java
@@ -20,43 +20,35 @@ public class GsonUtil {
 
 
     public static String toJson(Object o) {
-        Gson gson = new Gson();
-        return gson.toJson(o);
+        return new Gson().toJson(o);
     }
 
     public static void toJson(Object o, Appendable appendable) {
-        Gson gson = new Gson();
-        gson.toJson(o, appendable);
+        new Gson().toJson(o, appendable);
     }
 
     public static <T> T fromJson(String json, Class<T> clazz) {
-        Gson gson = new Gson();
-        return gson.fromJson(json, clazz);
+        return new Gson().fromJson(json, clazz);
     }
 
     public static <T> T fromJson(InputStream inputStream, Class<T> clazz) {
-        Gson gson = new Gson();
-        return gson.fromJson(new InputStreamReader(inputStream), clazz);
+        return new Gson().fromJson(new InputStreamReader(inputStream), clazz);
     }
 
     public static <T> T fromJson(Reader reader, Class<T> clazz) {
-        Gson gson = new Gson();
-        return gson.fromJson(reader, clazz);
+        return new Gson().fromJson(reader, clazz);
     }
 
     public static <T> T fromJson(JsonObject jsonObject, Class<T> clazz) {
-        Gson gson = new Gson();
-        return gson.fromJson(jsonObject, clazz);
+        return new Gson().fromJson(jsonObject, clazz);
     }
 
     public static <T, U extends TypeToken<List<T>>> List<T> fromJsonArray(String json, U typeToken) {
-        Gson gson = new Gson();
-        return gson.fromJson(json, typeToken.getType());
+        return new Gson().fromJson(json, typeToken.getType());
     }
 
     public static JsonElement fromString(String body) {
-        JsonParser jsonParser = new JsonParser();
-        return jsonParser.parse(body);
+        return new JsonParser().parse(body);
     }
 
     public static JsonElement get(JsonElement jsonElement, String memberName) {
@@ -95,8 +87,7 @@ public class GsonUtil {
 
 
     public static Map<String, Object> fromJsonMap(String json) {
-        Gson gson = new Gson();
 
-        return gson.fromJson(json, new TypeToken<Map<String, String>>(){}.getType());
+        return new Gson().fromJson(json, new TypeToken<Map<String, String>>() {}.getType());
     }
 }
