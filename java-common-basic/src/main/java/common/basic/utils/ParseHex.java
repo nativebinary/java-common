e969a399368b2599bdeb27fba633327755dde6c9
millpoo <radiantjb@gmail.com>
2014-08-13 02:59:08 +0900

    ParseHex getHexStr add

--- java-common-basic/src/main/java/common/basic/utils/ParseHex.java
+++ java-common-basic/src/main/java/common/basic/utils/ParseHex.java
@@ -7,6 +7,33 @@ import java.util.ArrayList;
  */
 public class ParseHex {
 
+    public static String getHexStr(String s) {
+
+        ArrayList<String> list = new ArrayList<String>();
+        boolean check = true;
+
+        for(int i=0; i<s.length(); i++) {
+
+            int ch = (int)s.charAt(i);
+
+            if(ch >= 48 && ch < 58 || ch >= 65 && ch < 71 || ch >= 97 && ch < 103) {
+                list.add(getHex(Integer.toString(ch)));
+            } else {
+                check = false;
+                break;
+            }
+        }
+
+        if(!check) return "16진수 변환에 실피해였습니다. 입력 값을 확인해 주세요.";
+
+        String makeHaxNum = makeHex(s);
+        String checkHaxNum = getHex(makeHaxNum);
+
+        if(!checkHaxNum.equals(s)) return "16진수가 아닙니다. 입력 값을 확인해 주세요.";
+
+        return makeHaxNum;
+    }
+
     public static String getHex(String s) {
 
         ArrayList<String> list = new ArrayList<String>();
@@ -65,26 +92,55 @@ public class ParseHex {
         return returnNum;
     }
 
-    public static String getHexStr(String s) {
+    public static String makeHex(String s) {
 
-        ArrayList<String> list = new ArrayList<String>();
-        String returnNum = "";
-        boolean check = true;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int sum = 0;
 
         for(int i=0; i<s.length(); i++) {
 
-            int ch = (int)s.charAt(i);
+            switch (s.charAt(i)) {
 
-            if(ch >= 48 && ch < 58 || ch >= 65 && ch < 71 || ch >= 97 && ch < 103) {
-                returnNum += getHex(Integer.toString(ch));
-            } else {
-                check = false;
-                break;
+                case 'A':
+                case 'a':
+                    list.add(10);
+                    break;
+
+                case 'B':
+                case 'b':
+                    list.add(11);
+                    break;
+
+                case 'C':
+                case 'c':
+                    list.add(12);
+                    break;
+
+                case 'D':
+                case 'd':
+                    list.add(13);
+                    break;
+
+                case 'E':
+                case 'e':
+                    list.add(14);
+                    break;
+
+                case 'F':
+                case 'f':
+                    list.add(15);
+                    break;
+
+                default:
+                    list.add(Integer.parseInt(Character.toString(s.charAt(i))));
             }
         }
 
-        if(!check) return "16진수 변환에 실피해였습니다. 입력 값을 확인해 주세요.";
+        for(int i=0; i<list.size(); i++) {
 
-        return returnNum;
+            sum += list.get(i) * (int)Math.pow(16, list.size() - 1 - i);
+        }
+
+        return Integer.toString(sum);
     }
 }
