317331355f9b2ab39bf856b37601e9d43a23087c
aha00a <aha00a@gmail.com>
2014-08-12 16:45:08 +0900

    add TestCase for common.basic.utils.Grep

---
+++ java-common-basic/src/test/java/common/basic/utils/GrepTest.java
@@ -0,0 +1,58 @@
+package common.basic.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GrepTest {
+    final String input =
+            "I am a boy!\n" +
+            "You are a girl?\n" +
+            "He is my father.\n" +
+            "She is her mother~\n" +
+            "GrepTDDCode";
+
+    @Test
+    public void testExecute() throws Exception {
+
+        Assert.assertEquals(
+                "You are a girl?",
+                Grep.execute(input, "girl"));
+
+        Assert.assertEquals(
+                "He is my father.\n" +
+                "She is her mother~",
+                Grep.execute(input, "is"));
+
+        Assert.assertEquals(
+                "He is my father.\n" +
+                "She is her mother~",
+                Grep.execute(input, "her"));
+
+        Assert.assertEquals(
+                "She is her mother~",
+                Grep.execute(input, "~"));
+    }
+
+    @Test
+    public void testExecuteInvert() throws Exception {
+
+        Assert.assertEquals(
+                "I am a boy!\n" +
+                "He is my father.\n" +
+                "She is her mother~\n" +
+                "GrepTDDCode",
+                Grep.executeInvert(input, "girl"));
+
+        Assert.assertEquals(
+                "I am a boy!\n" +
+                "You are a girl?\n" +
+                "GrepTDDCode",
+                Grep.executeInvert(input, "is"));
+
+        Assert.assertEquals(
+                "I am a boy!\n" +
+                "You are a girl?\n" +
+                "GrepTDDCode",
+                Grep.executeInvert(input, "her"));
+    }
+}
