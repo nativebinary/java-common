c4e5bb1940d46b37ea90420a45fcbfa29ae5ea79
millpoo <radiantjb@gmail.com>
2014-08-12 17:14:03 +0900

    ParseHex add

---
+++ java-common-basic/src/main/java/common/basic/utils/ParseHex.java
@@ -0,0 +1,12 @@
+package common.basic.utils;
+
+/**
+ * Created by jaebeomi on 8/12/14.
+ */
+public class ParseHex {
+
+    public static boolean parseHex(String s) {
+
+        return true;
+    }
+}
