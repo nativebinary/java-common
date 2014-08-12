7402972fbc75f54712355d1d264ae83ff3bfac3a
millpoo <radiantjb@gmail.com>
2014-08-13 05:14:19 +0900

    ParseHex refactoring

--- java-common-basic/src/main/java/common/basic/utils/ParseHex.java
+++ java-common-basic/src/main/java/common/basic/utils/ParseHex.java
@@ -7,140 +7,58 @@ import java.util.ArrayList;
  */
 public class ParseHex {
 
-    public static String getHexStr(String s) {
+    public static long parseHex(String hexString) throws Exception {
 
-        ArrayList<String> list = new ArrayList<String>();
-        boolean check = true;
+        if (null == hexString || 0 == hexString.length())
+            throw new Exception("empty string");
 
-        for(int i=0; i<s.length(); i++) {
+        if (16 < hexString.length())
+            throw new Exception("too long string");
 
-            int ch = (int)s.charAt(i);
+        long value = 0;
 
-            if(ch >= 48 && ch < 58 || ch >= 65 && ch < 71 || ch >= 97 && ch < 103) {
-                list.add(getHex(Integer.toString(ch)));
+        for (int i = 0; i < hexString.length(); ++i) {
+            char hexChar = hexString.charAt(i);
+
+            long decimalFromHex;
+            if ('0' <= hexChar && hexChar <= '9') {
+                decimalFromHex = hexChar - '0';
+            } else if ('a' <= hexChar && hexChar <= 'f') {
+                decimalFromHex = hexChar - 'a' + 10;
+            } else if ('A' <= hexChar && hexChar <= 'F') {
+                decimalFromHex = hexChar - 'A' + 10;
+
             } else {
-                check = false;
-                break;
+                throw new Exception("invalid hex string");
             }
+
+            value *= 16;
+            value += decimalFromHex;
         }
 
-        if(!check) return "16진수 변환에 실피해였습니다. 입력 값을 확인해 주세요.";
-
-        String makeHaxNum = makeHex(s);
-        String checkHaxNum = getHex(makeHaxNum);
-
-        if(!checkHaxNum.equals(s)) return "16진수가 아닙니다. 입력 값을 확인해 주세요.";
-
-        return makeHaxNum;
+        return value;
     }
 
-    public static String getHex(String s) {
+    public static String toHex(long value) {
 
-        ArrayList<String> list = new ArrayList<String>();
-        String returnNum = "";
-        if(s.contains("-")) {
-            returnNum += "-";
-            s = s.replace("-", "");
-        }
-        int num = Integer.parseInt(s);
-        int rest;
-        int value = num;
+        if (0 == value)
+            return "0";
 
-        do {
+        StringBuilder hexString = new StringBuilder();
+        while (0 != value) {
 
-            rest = value % 16;
-            value = value / 16;
-            list.add(Integer.toString(rest));
-        } while(value >= 16);
-
-        if(num > 15) list.add(Integer.toString(value));
-
-        for(int i=0; i<list.size(); i++) {
-
-            switch (Integer.parseInt(list.get(i))) {
-
-                case 10:
-                    list.set(i, "A");
-                    break;
-
-                case 11:
-                    list.set(i, "B");
-                    break;
-
-                case 12:
-                    list.set(i, "C");
-                    break;
-
-                case 13:
-                    list.set(i, "D");
-                    break;
-
-                case 14:
-                    list.set(i, "E");
-                    break;
-
-                case 15:
-                    list.set(i, "F");
-                    break;
+            long mod = value & 0x000000000000000f;
+            char hexValue;
+            if (mod < 10) {
+                hexValue = (char) ('0' + mod);
+            } else {
+                hexValue = (char) ('a' + mod - 10);
             }
+            hexString.insert(0, hexValue);
+
+            value = (value >> 4) & 0x0fffffffffffffffL;
         }
 
-        for(int i=list.size()-1; i>=0; i--) {
-            returnNum += list.get(i);
-        }
-
-        return returnNum;
-    }
-
-    public static String makeHex(String s) {
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int sum = 0;
-
-        for(int i=0; i<s.length(); i++) {
-
-            switch (s.charAt(i)) {
-
-                case 'A':
-                case 'a':
-                    list.add(10);
-                    break;
-
-                case 'B':
-                case 'b':
-                    list.add(11);
-                    break;
-
-                case 'C':
-                case 'c':
-                    list.add(12);
-                    break;
-
-                case 'D':
-                case 'd':
-                    list.add(13);
-                    break;
-
-                case 'E':
-                case 'e':
-                    list.add(14);
-                    break;
-
-                case 'F':
-                case 'f':
-                    list.add(15);
-                    break;
-
-                default:
-                    list.add(Integer.parseInt(Character.toString(s.charAt(i))));
-            }
-        }
-
-        for(int i=0; i<list.size(); i++) {
-
-            sum += list.get(i) * (int)Math.pow(16, list.size() - 1 - i);
-        }
-
-        return Integer.toString(sum);
+        return hexString.toString();
     }
 }
