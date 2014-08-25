92989b7b6f9bfa877cd6ca45bb99f41896d8eecc
millpoo <radiantjb@gmail.com>
2014-08-25 17:09:15 +0900

    URLUtilTest groovy language decodeURIComponentIfEncoded method.

--- java-common-basic/src/test/groovy/common/basic/utils/URLUtilTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/utils/URLUtilTest.groovy
@@ -56,13 +56,26 @@ class URLUtilTest extends Specification {
             str                         ||  result
             "test"                      ||  "test"
             "test input url code"       ||  "test+input+url+code"
+            "http://www.test.com"       ||  "http%3A%2F%2Fwww.test.com"
             "동해물과 백두산이 마르고 닳도록"   ||  "%EB%8F%99%ED%95%B4%EB%AC%BC%EA%B3%BC+%EB%B0%B1%EB%91%90%EC%82%B0%EC%9D%B4+%EB%A7%88%EB%A5%B4%EA%B3%A0+%EB%8B%B3%EB%8F%84%EB%A1%9D"
+            "https://www.애국가.com"      ||  "https%3A%2F%2Fwww.%EC%95%A0%EA%B5%AD%EA%B0%80.com"
             ""                          ||  ""
             null                        ||  ""
     }
 
     def "DecodeURIComponentIfEncoded"() {
 
+        expect:
+            URLUtil.decodeURIComponentIfEncoded(url) == result
+
+        where:
+            url                                                     ||  result
+            "http://www.test.com"                                   ||  "http://www.test.com"
+            "http://www.test.com?a%20=%20tp&b=%7c"                  ||  "http://www.test.com?a%20=%20tp&b=%7c"
+            "http%3A%2F%2Fwww.test.com?a%20=%20tp&b=%7c"            ||  "http://www.test.com?a = tp&b=|"
+            "http%3A%2F%2Fwww.test.com"                             ||  "http://www.test.com"
+            "https%3A%2F%2Fwww.애국가.com"                            ||  "https://www.애국가.com"
+            "https%3A%2F%2Fwww.%EC%95%A0%EA%B5%AD%EA%B0%80.com"     ||  "https://www.애국가.com"
     }
 
     def "Decode"() {
