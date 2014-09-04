c84f5446a7cd3d2ca3ddb2f3c115f702d57d1e44
aha00a <aha00a@gmail.com>
2014-09-04 19:18:08 +0900

    add skeleton code for LeftTopRightBottomTest

---
+++ java-common-basic/src/test/groovy/common/basic/geometiries/LeftTopRightBottomTest.groovy
@@ -0,0 +1,17 @@
+package common.basic.geometiries
+
+import spock.lang.Specification
+
+class LeftTopRightBottomTest extends Specification {
+    def "Equals"() {
+        expect: true;
+    }
+
+    def "HashCode"() {
+        expect: true;
+    }
+
+    def "ToString"() {
+        expect: true;
+    }
+}
