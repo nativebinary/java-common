69b67ccc5c832b1e01cc0f2d0bea7345f6936e88
millpoo <millpoo@nativebinary.com>
2014-09-05 15:58:15 +0900

    LeftTopRightBottomTest equals parameter null add.

--- java-common-basic/src/test/groovy/common/basic/geometiries/LeftTopRightBottomTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/geometiries/LeftTopRightBottomTest.groovy
@@ -11,25 +11,33 @@ class LeftTopRightBottomTest extends Specification {
         ltrbLhs.equals(ltrbRhs) == result
 
         where:
-        ltrbLhs                     ||  ltrbRhs                                 ||   result
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom(10)              ||   true
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom(10, 10, 10, 10)  ||   true
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom(11)              ||   false
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom(10, 10, 20, 10)  ||   false
+        ltrbLhs                                     ||  ltrbRhs                                     ||  result
+        new LeftTopRightBottom(10)                  ||  new LeftTopRightBottom(10)                  ||  true
+        new LeftTopRightBottom(10)                  ||  new LeftTopRightBottom(10, 10, 10, 10)      ||  true
+        new LeftTopRightBottom(10)                  ||  new LeftTopRightBottom(11)                  ||  false
+        new LeftTopRightBottom(10)                  ||  new LeftTopRightBottom(10, 10, 20, 10)      ||  false
 
-        new LeftTopRightBottom(22)  ||  new LeftTopRightBottom(22)              ||   true
-        new LeftTopRightBottom(22)  ||  new LeftTopRightBottom(22, 22, 22, 22)  ||   true
-        new LeftTopRightBottom(22)  ||  new LeftTopRightBottom(22, 11, 22, 22)  ||   false
+        new LeftTopRightBottom(22)                  ||  new LeftTopRightBottom(22)                  ||  true
+        new LeftTopRightBottom(22)                  ||  new LeftTopRightBottom(22, 22, 22, 22)      ||  true
+        new LeftTopRightBottom(22)                  ||  new LeftTopRightBottom(22, 11, 22, 22)      ||  false
 
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom(10.0)            ||   false
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom(10.0F)           ||   false
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom(10L)             ||   false
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom("10")            ||   false
-        new LeftTopRightBottom(10)  ||  new LeftTopRightBottom('10')            ||   false
+        new LeftTopRightBottom(10)                  ||  new LeftTopRightBottom(10.0)                ||  false
+        new LeftTopRightBottom(10)                  ||  new LeftTopRightBottom(10.0F)               ||  false
+        new LeftTopRightBottom(10)                  ||  new LeftTopRightBottom(10L)                 ||  false
+        new LeftTopRightBottom(10)                  ||  null                                        ||  false
+        new LeftTopRightBottom(null)                ||  null                                        ||  false
+        new LeftTopRightBottom(null)                ||  new LeftTopRightBottom(null)                ||  true
+        new LeftTopRightBottom(10, 10, 10, null)    ||  new LeftTopRightBottom(10, 10, 10, null)    ||  true
     }
 
     def "HashCode"() {
 
+//
+//        expect:
+//        ltrbLhs.hashCode() == result
+//
+//        where:
+//        ltrbLhs     ||  result
     }
 
     def "ToString"() {
