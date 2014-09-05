62ce842c2e670050ed908a2f1c330327fc6d9b22
millpoo <millpoo@nativebinary.com>
2014-09-05 14:39:28 +0900

    LeftTopRightBottom groovy test code and equals method.

--- java-common-basic/src/test/groovy/common/basic/geometiries/LeftTopRightBottomTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/geometiries/LeftTopRightBottomTest.groovy
@@ -3,8 +3,26 @@ package common.basic.geometiries
 import spock.lang.Specification
 
 class LeftTopRightBottomTest extends Specification {
+
+    @SuppressWarnings("GroovyPointlessBoolean")
     def "Equals"() {
-        expect: true;
+
+        expect:
+        new LeftTopRightBottom(10).equals(obj) == result
+
+        where:
+        obj                                     ||  result
+        new LeftTopRightBottom(10)              ||  true
+        new LeftTopRightBottom(10, 10, 10, 10)  ||  true
+        "str"                                   ||  false
+        10                                      ||  false
+        new LeftTopRightBottom(11)              ||  false
+        new LeftTopRightBottom(10, 10, 10, 11)  ||  false
+        new LeftTopRightBottom(10.0)            ||  false
+        new LeftTopRightBottom(10.0F)           ||  false
+        new LeftTopRightBottom(10L)             ||  false
+        new LeftTopRightBottom("10")            ||  false
+        new LeftTopRightBottom('10')            ||  false
     }
 
     def "HashCode"() {
