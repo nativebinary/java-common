96d78f8ccadf6adc09d9c212cf4344d956832839
millpoo <millpoo@nativebinary.com>
2014-09-10 14:37:41 +0900

    PointTest tdd and multiply method and add case for negative value

--- java-common-basic/src/test/groovy/common/basic/geometiries/PointTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/geometiries/PointTest.groovy
@@ -23,15 +23,17 @@ class PointTest extends Specification {
         point.negate() == result
 
         where:
-        point            ||  result
-        new Point(10, 10)   ||  new Point(-10, -10)
-        new Point(15, 20)   ||  new Point(-15, -20)
-        new Point(47, 8)    ||  new Point(-47, -8)
-        new Point(-10, -10) ||  new Point(10, 10)
-        new Point(-15, -20) ||  new Point(15, 20)
-        new Point(-47, -8)  ||  new Point(47, 8)
-        new Point(10, -10)  ||  new Point(-10, 10)
-        new Point(-15, 20)  ||  new Point(15, -20)
+        point                   ||  result
+        new Point(10, 10)       ||  new Point(-10, -10)
+        new Point(15, 20)       ||  new Point(-15, -20)
+        new Point(47, 8)        ||  new Point(-47, -8)
+        new Point(30, 0)        ||  new Point(-30, 0)
+        new Point(-82, -51)     ||  new Point(82, 51)
+        new Point(11, -72)      ||  new Point(-11, 72)
+        new Point(-39, 29)      ||  new Point(39, -29)
+        new Point(100, 200)     ||  new Point(-100, -200)
+        new Point(3678, -98)    ||  new Point(-3678, 98)
+        new Point(-9834, -9999) ||  new Point(9834, 9999)
     }
 
     def "Multiply"() {
@@ -40,10 +42,17 @@ class PointTest extends Specification {
         point.multiply(param) == result
 
         where:
-        point               ||  param   ||  result
-        new Point(10, 10)   ||  5       ||  new Point(10 * 5, 10 * 5)
-        new Point(15, 20)   ||  13      ||  new Point(15 * 13, 20 * 13)
-        new Point(47, 8)    ||  -6      ||  new Point(47 * -6, 8 * -6)
+        point                   ||  param   ||  result
+        new Point(10, 10)       ||  5       ||  new Point(10 * 5, 10 * 5)
+        new Point(15, 20)       ||  13      ||  new Point(15 * 13, 20 * 13)
+        new Point(47, 8)        ||  -6      ||  new Point(47 * (-6), 8 * (-6))
+        new Point(30, 0)        ||  -9      ||  new Point(30 * (-9), 0)
+        new Point(-82, -51)     ||  76      ||  new Point(-82 * 76, -51 * 76)
+        new Point(11, -72)      ||  11      ||  new Point(11 * 11, -72 * 11)
+        new Point(-39, 29)      ||  35      ||  new Point(-39 * 35, 29 * 35)
+        new Point(100, 200)     ||  -96     ||  new Point(100 * (-96), 200 * (-96))
+        new Point(3678, -98)    ||  27      ||  new Point(3678 * 27, -98 * 27)
+        new Point(-9834, -9999) || -8       ||  new Point(-9834 * (-8), -9999 * (-8))
     }
 
     def "DivideBy"() {
