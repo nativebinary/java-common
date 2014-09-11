e46177e141997c6888f9753580b9f3f407e60ba0
millpoo <millpoo@nativebinary.com>
2014-09-11 18:47:17 +0900

    RectTest tdd and fromLeftTopRightBottom method.

---
+++ java-common-basic/src/test/groovy/common/basic/geometiries/RectTest.groovy
@@ -0,0 +1,65 @@
+package common.basic.geometiries
+
+import spock.lang.Specification
+
+class RectTest extends Specification {
+
+    def "FromLeftTopRightBottom"() {
+
+        expect:
+        Rect.fromLeftTopRightBottom(left, top, right, bottom) == result
+
+        where:
+        left    ||  top ||  right   ||  bottom  ||  result
+        10      ||  10  ||  10      ||  10      ||  new Rect(new Point(10, 10), new Size(0, 0))
+        15      ||  52  ||  49      ||  -12     ||  new Rect(new Point(15, 52), new Size(34, -64))
+        23      ||  -18 ||  27      ||  36      ||  new Rect(new Point(23, -18), new Size(4, 54))
+        -126    ||  -92 ||  39      ||  67      ||  new Rect(new Point(-126, -92), new Size(165, 159))
+        88      ||  27  ||  2       ||  27      ||  new Rect(new Point(88, 27), new Size(-86, 0))
+        -96     ||  42  ||  14      ||  33      ||  new Rect(new Point(-96, 42), new Size(110, -9))
+    }
+
+    def "Width"() {
+
+    }
+
+    def "Height"() {
+
+    }
+
+    def "Left"() {
+
+    }
+
+    def "Top"() {
+
+    }
+
+    def "Right"() {
+
+    }
+
+    def "Bottom"() {
+
+    }
+
+    def "Center"() {
+
+    }
+
+    def "Deflate"() {
+
+    }
+
+    def "Contains"() {
+
+    }
+
+    def "Offset"() {
+
+    }
+
+    def "ToString"() {
+
+    }
+}
