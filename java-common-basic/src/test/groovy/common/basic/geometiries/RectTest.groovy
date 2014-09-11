8b82efa62e0f672be91df8db9af57e6d49718735
millpoo <millpoo@nativebinary.com>
2014-09-11 18:58:16 +0900

    RectTest tdd and width method.

--- java-common-basic/src/test/groovy/common/basic/geometiries/RectTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/geometiries/RectTest.groovy
@@ -21,6 +21,17 @@ class RectTest extends Specification {
 
     def "Width"() {
 
+        expect:
+        rect.width() == result
+
+        where:
+        rect                                                ||  result
+        new Rect(new Point(10, 10), new Size(0, 0))         ||  0
+        new Rect(new Point(15, 52), new Size(34, -64))      ||  34
+        new Rect(new Point(23, -18), new Size(4, 54))       ||  4
+        new Rect(new Point(-126, -92), new Size(165, 159))  ||  165
+        new Rect(new Point(88, 27), new Size(-86, 0))       ||  -86
+        new Rect(new Point(-96, 42), new Size(110, -9))     ||  110
     }
 
     def "Height"() {
