6b01d36a227ffbf740f35aec0f1dae1c9b395160
millpoo <millpoo@nativebinary.com>
2014-09-11 13:59:31 +0900

    PointDTest tdd and divideBy method

--- java-common-basic/src/test/groovy/common/basic/geometiries/PointDTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/geometiries/PointDTest.groovy
@@ -54,6 +54,18 @@ class PointDTest extends Specification {
 
     def "DivideBy"() {
 
+        expect:
+        point.divideBy(param) == result
+
+        where:
+        point                       ||  param       ||  result
+        new PointD(10, 10)          ||  10.0        ||  new PointD(1.0, 1.0)
+        new PointD(10.0, 10.0)      ||  10.0        ||  new PointD(1.0, 1.0)
+        new PointD(26.98, 6.89)     ||  7.62        ||  new PointD(3.540682414698163, 0.9041994750656167)
+        new PointD(-56.29, -962.2)  ||  9.6         ||  new PointD(-5.863541666666666, -100.22916666666667)
+        new PointD(22.225, -16.204) ||  -15.25      ||  new PointD(-1.457377049180328, 1.0625573770491803)
+        new PointD(-129.0, -77.0)   ||  23.58       ||  new PointD(-5.470737913486006, -3.2654792196776934)
+        new PointD(86.25, -98.2)    ||  3.5         ||  new PointD(24.642857142857142, -28.057142857142857)
     }
 
     def "Delta"() {
