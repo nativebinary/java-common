8e12c125eced436bafa496fceaa0172f96dc9c67
millpoo <millpoo@nativebinary.com>
2014-09-11 19:01:14 +0900

    RectTest tdd and height method.

--- java-common-basic/src/test/groovy/common/basic/geometiries/RectTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/geometiries/RectTest.groovy
@@ -36,6 +36,17 @@ class RectTest extends Specification {
 
     def "Height"() {
 
+        expect:
+        rect.height() == result
+
+        where:
+        rect                                                ||  result
+        new Rect(new Point(10, 10), new Size(0, 0))         ||  0
+        new Rect(new Point(15, 52), new Size(34, -64))      ||  -64
+        new Rect(new Point(23, -18), new Size(4, 54))       ||  54
+        new Rect(new Point(-126, -92), new Size(165, 159))  ||  159
+        new Rect(new Point(88, 27), new Size(-86, 72))      ||  72
+        new Rect(new Point(-96, 42), new Size(110, -9))     ||  -9
     }
 
     def "Left"() {
