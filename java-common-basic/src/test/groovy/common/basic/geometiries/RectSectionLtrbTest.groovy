ffda9f843fb561eca667deadeecc51b65a4083de
millpoo <millpoo@nativebinary.com>
2014-09-15 13:46:52 +0900

    test case isHorizontal method.

--- java-common-basic/src/test/groovy/common/basic/geometiries/RectSectionLtrbTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/geometiries/RectSectionLtrbTest.groovy
@@ -37,8 +37,29 @@ class RectSectionLtrbTest extends Specification {
         new SizeF(89, 76)                   ||  new PointF(45, 35)                      ||  RectSectionLtrb.Top
     }
 
+    @SuppressWarnings("GroovyPointlessBoolean")
     def "IsHorizontal"() {
+        expect:
+        RectSectionLtrb.getTouchArea(size, point).isHorizontal() == result
 
+        where:
+        size                                ||  point                                   ||  result
+        new SizeF(78, 26)                   ||  new PointF(20, 14)                      ||  false
+        new SizeF(85, 35)                   ||  new PointF(43, 18)                      ||  false
+        new SizeF(98, 42)                   ||  new PointF(75, 19)                      ||  true
+        new SizeF(69, 14)                   ||  new PointF(55, 11)                      ||  true
+        new SizeF(58, 48)                   ||  new PointF(13, 22)                      ||  true
+        new SizeF(27, 12)                   ||  new PointF(5, 11)                       ||  true
+        new SizeF(49, 33)                   ||  new PointF(13, 15)                      ||  false
+        new SizeF(89, 76)                   ||  new PointF(45, 35)                      ||  false
+        new SizeF(30, 36)                   ||  new PointF(28, 30)                      ||  false
+        new SizeF(18, 27)                   ||  new PointF(12, 13)                      ||  true
+        new SizeF(26, 52)                   ||  new PointF(25, 8)                       ||  false
+        new SizeF(48, 69)                   ||  new PointF(22, 34)                      ||  true
+        new SizeF(54, 77)                   ||  new PointF(26, 15)                      ||  false
+        new SizeF(86, 94)                   ||  new PointF(42, 47)                      ||  true
+        new SizeF(67, 82)                   ||  new PointF(35, 41)                      ||  true
+        new SizeF(78, 104)                  ||  new PointF(26, 82)                      ||  false
     }
 
     def "IsBefore"() {
