d13478aed41dd76aa1b4d57cdd692ca3f79ee8e0
roadster <roadster@nativebinary.com>
2014-09-29 15:19:51 +0900

    remove comment

--- java-common-basic/src/test/groovy/common/CommonInitTest.groovy
+++ java-common-basic/src/test/groovy/common/CommonInitTest.groovy
@@ -2,9 +2,6 @@ package common
 
 import spock.lang.Specification
 
-/**
- * Created by roadster on 2014. 9. 29..
- */
 class CommonInitTest extends Specification {
     def "ctor"() {
         when: new CommonInit()
