31f6116cb4d8f39dc49545dd47efe0e7ab839c51
millpoo <millpoo@nativebinary.com>
2015-03-16 21:55:20 +0900

    add test case to startsWith method

--- java-common-basic/src/test/groovy/common/basic/utils/ArrayUtilTest.groovy
+++ java-common-basic/src/test/groovy/common/basic/utils/ArrayUtilTest.groovy
@@ -21,4 +21,24 @@ class ArrayUtilTest extends Specification {
         false   ||  new ArrayList<String>()
         false   ||  new ArrayList<String>().add("1")
     }
+
+    def "startsWith"() {
+        expect:
+        i == ArrayUtil.startsWith(array1 as String[], array2 as String[])
+
+        where:
+        i       ||  array1                  ||  array2
+        true    ||  ["1"]                   ||  ["1"]
+        false   ||  ["1"]                   ||  ["2"]
+        true    ||  ["1", "2"]              ||  ["1"]
+        true    ||  ["1", "2"]              ||  ["1", "2"]
+        false   ||  ["1", "2"]              ||  ["2", "1"]
+        false   ||  ["1", "2"]              ||  ["1", "1"]
+        true    ||  ["1", "2", "3"]         ||  ["1"]
+        true    ||  ["1", "2", "3"]         ||  ["1", "2"]
+        true    ||  ["1", "2", "3"]         ||  ["1", "2", "3"]
+        false   ||  ["1", "2", "3"]         ||  ["3", "2", "1"]
+        false   ||  ["1", "2", "3"]         ||  ["1", "3", "2"]
+        false   ||  ["1", "2", "3"]         ||  ["1", "3"]
+    }
 }
