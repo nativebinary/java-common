02455ace2b525bb133d6b6922dd635689c437f38
silsol <silsol@gmail.com>
2015-05-06 05:57:39 +0900

    ReflectionUtil improve

--- java-common-basic/src/main/java/common/basic/utils/ReflectionUtil.java
+++ java-common-basic/src/main/java/common/basic/utils/ReflectionUtil.java
@@ -1,5 +1,6 @@
 package common.basic.utils;
 
+import common.basic.interfaces.ICallbackTransform;
 import common.basic.interfaces.IPredicator;
 import common.basic.logs.Logger;
 
@@ -12,6 +13,7 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -420,6 +422,197 @@ public class ReflectionUtil {
         }
         return Class.forName(className);
     }
+
+
+    ////////////////////////////////
+
+
+
+    public static <T, U extends Annotation> void setFieldByStringArray(T instance, Field field, String[] arrayString, Class<U> annotationClass) throws IllegalAccessException, InstantiationException {
+
+        if (null == arrayString || 0 == arrayString.length)
+            return;
+
+        field.setAccessible(true);
+
+        final Class<?> type = field.getType();
+
+        if(type.isPrimitive())
+        {
+            String stringValue = arrayString[0];
+
+            if (int.class.equals(type)) {
+                field.setInt(instance, IntUtil.parseInt(stringValue, 0));
+            } else if (long.class.equals(type)) {
+                field.setLong(instance, LongUtil.parseLong(stringValue, 0));
+            } else if (float.class.equals(type)) {
+                field.setFloat(instance, Float.parseFloat(stringValue));
+            } else if (double.class.equals(type)) {
+                field.setDouble(instance, Double.parseDouble(stringValue));
+            } else if (boolean.class.equals(type)) {
+                field.setBoolean(instance, BooleanUtil.parse(stringValue));
+            } else if (byte.class.equals(type)) {
+                field.setByte(instance, Byte.parseByte(stringValue));
+            } else if (short.class.equals(type)) {
+                field.setShort(instance, Short.parseShort(stringValue));
+            } else if (char.class.equals(type)) {
+                field.setChar(instance, stringValue.charAt(0));
+            } else {
+                field.set(instance, stringValue);
+            }
+        } else if(type.equals(String.class)) {
+            String stringValue = arrayString[0];
+            field.set(instance, stringValue);
+        } else if(type.equals(Date.class)) {
+            String stringValue = arrayString[0];
+            Date parse = DateUtil.yyyyMMddHHmmss(stringValue, null);
+            if (null != parse)
+                field.set(instance, parse);
+
+        } else if (type.equals(java.util.List.class)) {
+            ParameterizedType stringListType = (ParameterizedType) field.getGenericType();
+            Class<?> genericClass = (Class<?>) stringListType.getActualTypeArguments()[0];
+
+            if (genericClass.isEnum()) {
+                List<Enum> objects = new ArrayList<Enum>();
+                for (String singleValue : arrayString) {
+                    Enum parse = EnumUtil.parse((Class<? extends Enum>) genericClass, singleValue);
+                    objects.add(parse);
+                }
+                field.set(instance, objects);
+            } else if (genericClass.equals(String.class)){
+                List<String> objects = new ArrayList<String>();
+                Collections.addAll(objects, arrayString);
+                field.set(instance, objects);
+            } else {
+                if (Integer.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Integer>() {
+                        @Override
+                        public Integer transform(String s) {
+                            return IntUtil.parseInt(s, 0);
+                        }
+                    }));
+                } else if (Long.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Long>() {
+                        @Override
+                        public Long transform(String s) {
+                            return LongUtil.parseLong(s, 0);
+                        }
+                    }));
+                } else if (Float.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Float>() {
+                        @Override
+                        public Float transform(String s) {
+                            return Float.parseFloat(s);
+                        }
+                    }));
+                } else if (Double.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Double>() {
+                        @Override
+                        public Double transform(String s) {
+                            return Double.parseDouble(s);
+                        }
+                    }));
+                } else if (Boolean.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Boolean>() {
+                        @Override
+                        public Boolean transform(String s) {
+                            return BooleanUtil.parse(s);
+                        }
+                    }));
+                } else if (Byte.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Byte>() {
+                        @Override
+                        public Byte transform(String s) {
+                            return Byte.parseByte(s);
+                        }
+                    }));
+                } else if (Short.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Short>() {
+                        @Override
+                        public Short transform(String s) {
+                            return Short.parseShort(s);
+                        }
+                    }));
+                } else if (Character.class.equals(type)) {
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Character>() {
+                        @Override
+                        public Character transform(String s) {
+                            if (StringUtil.isNullOrEmpty(s))
+                                return null;
+
+                            return s.charAt(0);
+                        }
+                    }));
+                } else {
+                    Logger.e(field, arrayString);
+                    field.set(instance, ListUtil.transform(arrayString, new ICallbackTransform<String, Object>() {
+                        @Override
+                        public Object transform(String s) {
+                            return s;
+                        }
+                    }));
+                }
+
+
+            }
+        } else if(type.isEnum()) {
+            //noinspection unchecked
+            String stringValue = arrayString[0];
+            field.set(instance, EnumUtil.parse((Class<? extends Enum>) type, stringValue));
+        } else {
+            if(annotationClass == null)
+                return;
+
+            final Object o = type.newInstance();
+            String stringValue = arrayString[0];
+            setAnnotatedKeyFieldValue(o, annotationClass, stringValue);
+            field.set(instance, o);
+        }
+    }
+
+    public static <T, U extends Annotation> T bindFromStringStringArray(Class<T> clazz, Map<String, String[]> map, Class<U> annotationClass) {
+        final T t;
+        try {
+            t = clazz.getConstructor(new Class<?>[0]).newInstance();
+
+            List<Field> listFieldDeclaredRecursive = getListFieldDeclaredRecursive(clazz);
+
+            for (Field field : listFieldDeclaredRecursive) {
+                String fieldName = field.getName();
+
+                String[] value = map.get(fieldName);
+                if (null == value)
+                    continue;
+
+                if(Modifier.isTransient(field.getModifiers()))
+                    continue;
+
+                setFieldByStringArray(t, field, value, annotationClass);
+            }
+
+            return t;
+        }
+        catch (InstantiationException e) {
+            Logger.e(e);
+        }
+        catch (IllegalAccessException e) {
+            Logger.e(e);
+        }
+        catch (InvocationTargetException e) {
+            Logger.e(e);
+        }
+        catch (NoSuchMethodException e) {
+            Logger.e(e);
+        }
+
+        return null;
+    }
+
+    public static <T> T bindFromStringStringArray(Class<T> clazz, Map<String, String[]> map) {
+        return bindFromStringStringArray(clazz, map, null);
+    }
+
 
     public static <T> void invokeIfExists(T t, String method) {
         final Class<?> clazz = t.getClass();
